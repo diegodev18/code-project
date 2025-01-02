6589aeda30b6254d87f9e72fa4f2ff68ff2fed61
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-02 00:41:13 -0600

    Update Vercel adapter configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({}),
   integrations: [tailwind(), auth()]
 });
