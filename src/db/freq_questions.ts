520a5e31822984a79e7d923f39416b9ffdc36816
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-02 13:01:24 -0600

    Add frequently asked questions data and update help page to use new data source

---
+++ src/db/freq_questions.ts
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: "Como puedo acceder a mi cuenta?",
+        content: "Para acceder a tu cuenta, debes iniciar session con github, presionando el boton con el icono de la persona en la esquina superior derecha."
+    },
+    {
+        title: "Como accedo a un proyecto?",
+        content: "Para acceder a un proyecto, debes buscarlo en la barra de busqueda y presionar en su tarjeta."
+    },
+    {
+        title: "Es gratis?",
+        content: "Si, Code Project y todos los proyectos que se encuentran en la plataforma son completamente gratis. Tambien, puedes contribuir a la plataforma via Paypal."
+    }
+]
