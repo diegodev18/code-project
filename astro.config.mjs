64dbcdd3f9c13364368c73d33583b87ce1a0b1ff
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-01 23:25:04 -0600

    Add authentication integration using auth-astro with GitHub provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), auth()]
 });
