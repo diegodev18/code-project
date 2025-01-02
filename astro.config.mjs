3ac2416f099c2deeace441ad34fb03b2fcd87025
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-01 23:31:26 -0600

    Add Vercel adapter to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel';
 import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [tailwind(), auth()]
 });
