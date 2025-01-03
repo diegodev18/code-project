af5b26cf3aa9f549bd29e02cca72c881170e4db5
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-03 08:00:49 -0600

    Add initial profiles data for user management

---
+++ src/db/profiles.ts
@@ -0,0 +1,14 @@
+const profiles = [
+    {
+        acc_id: 1,
+        name: "Diego Sanchez",
+        id: "",
+        email: "diegosanchez.dev018@gmail.com",
+        img: "https://avatars.githubusercontent.com/u/175571311?v=4",
+        progress: [
+            { id_project: "git", status: 2 },
+        ]
+    }
+]
+
+export default profiles
