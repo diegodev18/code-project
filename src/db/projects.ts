295443dac8d1c8822e4426782bf03006349e6b05
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-05 10:09:13 -0600

    Add project data structure and update project page for dynamic task rendering

---
+++ src/db/projects.ts
@@ -0,0 +1,21 @@
+const projects = [
+    {
+        num: 1,
+        id: 'your-own-git',
+        title: 'Construye tu propio Git',
+        description: 'Construye tu propio sistema de control de versiones desde cero.',
+        tasks: [
+            {
+                num: 1,
+                id: 'introduction',
+                title: 'Introducción',
+                description: 'Construye tu propio sistema de control de versiones desde cero.',
+                md: {
+                    c: 'projects/your-own-git/c/introduction',
+                }
+            }
+        ]
+    }
+]
+
+export default projects
