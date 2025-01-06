4b27a30d45b165db81e1fb9e50119134dbec104a
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-06 12:42:05 -0600

    Refactor project detail retrieval by creating a new utility function for improved code organization and error handling

---
+++ src/utils/getProjectsContent.ts
@@ -0,0 +1,20 @@
+import { getCollection } from "astro:content";
+
+export default async function (name: string, lenguage: string, title: string) {
+
+    const collections = await getCollection('projects', (entry) => {
+        if (entry.id.slice(0, entry.id.lastIndexOf("/")) === `${name}/${lenguage}`) {
+            return entry;
+        }
+    });
+
+    const info = collections.find(task => task.id === `${name}/${lenguage}/${title}.md`);
+
+    if (!info || !collections) {
+        return null;
+    }
+    return {
+        info: info,
+        collections: collections
+    };
+}
