474e1d4195b1ed10b1a1994f5a7de1f779db7460
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-06 07:56:08 -0600

    Add Linux project and update projects list with new entries

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -1,4 +1,5 @@
 import GitIcon from "@/components/icons/brands/gitIcon.astro"
+import LinuxIcon from "@/components/icons/brands/linuxIcon.astro"
 
 const projects = [
     {
@@ -12,7 +13,43 @@ const projects = [
             component: GitIcon
         },
         href: '/projects/your-own-git/c/introduction'
-    }
+    },
+    {
+        num: 2,
+        id: 'your-own-shell',
+        title: 'Creando tu propio Shell',
+        description: 'Construye tu propio shell desde cero.',
+        icon: {
+            width: 80,
+            height: 80,
+            component: GitIcon
+        },
+        href: '/projects/your-own-git/c/introduction'
+    },
+    {
+        num: 3,
+        id: 'your-own-linux-distro',
+        title: 'Construye tu propia distribución de Linux',
+        description: 'Construye tu propia distribución de Linux desde cero.',
+        icon: {
+            width: 90,
+            height: 90,
+            component: LinuxIcon
+        },
+        href: '/projects/your-own-git/c/introduction'
+    },
+    {
+        num: 4,
+        id: 'your-own-git',
+        title: 'Construye tu propio Git',
+        description: 'Construye tu propio sistema de control de versiones desde cero.',
+        icon: {
+            width: 80,
+            height: 80,
+            component: GitIcon
+        },
+        href: '/projects/your-own-git/c/introduction'
+    },
 ]
 
 export default projects
