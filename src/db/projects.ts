3a90c1038ef610599e998bc60313839b604b7e82
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-07 11:06:39 -0600

    Enhance projectsSmallCard component with hover effect for project titles and remove unused project entries from database

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -38,42 +38,6 @@ const projects = [
         },
         href: '/projects/your-own-git/c/introduction'
     },
-    {
-        num: 4,
-        id: 'your-own-git',
-        title: 'Construye tu propio Git',
-        description: 'Construye tu propio sistema de control de versiones desde cero.',
-        icon: {
-            width: 80,
-            height: 80,
-            component: GitIcon
-        },
-        href: '/projects/your-own-git/c/introduction'
-    },
-    {
-        num: 2,
-        id: 'your-own-shell',
-        title: 'Creando tu propio Shell',
-        description: 'Construye tu propio shell desde cero.',
-        icon: {
-            width: 80,
-            height: 80,
-            component: GitIcon
-        },
-        href: '/projects/your-own-git/c/introduction'
-    },
-    {
-        num: 3,
-        id: 'your-own-linux-distro',
-        title: 'Construye tu propia distribución de Linux',
-        description: 'Construye tu propia distribución de Linux desde cero.',
-        icon: {
-            width: 90,
-            height: 90,
-            component: LinuxIcon
-        },
-        href: '/projects/your-own-git/c/introduction'
-    },
 ]
 
 export default projects
