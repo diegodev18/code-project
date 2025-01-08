bb27d0299cbf945bf8661f12c01fd853b9a37730
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-07 20:43:08 -0600

    Refactor getProjectsContent function to improve type safety and clarity

--- src/utils/getProjectsContent.ts
+++ src/utils/getProjectsContent.ts
@@ -2,13 +2,13 @@ import { getCollection } from "astro:content";
 
 export default async function (name: string, lenguage: string, title: string) {
 
-    const collections = await getCollection('projects', (entry) => {
+    const collections = await getCollection('projects', (entry: { id: string }) => {
         if (entry.id.slice(0, entry.id.lastIndexOf("/")) === `${name}/${lenguage}`) {
             return entry;
         }
     });
 
-    const info = collections.find(task => task.id === `${name}/${lenguage}/${title}.md`);
+    const info = collections.find((task: { id: string }) => task.id === `${name}/${lenguage}/${title}.md`);
 
     if (!info || !collections) {
         return null;
