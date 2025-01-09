ac7a4aa32be81ce255687f5a7aeea3c4a2d087ac
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-09 10:55:00 -0600

    Refactor progress calculation to return detailed project status and update href logic in project cards

--- src/utils/getProgressFromAProject.ts
+++ src/utils/getProgressFromAProject.ts
@@ -1,14 +1,25 @@
 import Projects from "@/db/projects";
 import Profiles from "@/db/profiles";
 
-export default function (id_project: string, id_user: number): number {
+export default function (id_project: string, id_user: number): { progress: number, percentaje: number, lenguage: string } {
     const project = Projects.find(project => project.id === id_project);
-    const progress = Profiles.find(profile => profile.acc_id === id_user)?.progress.find(progress => progress.id_project === id_project)?.status;
+    const progress = Profiles.find(profile => profile.acc_id === id_user)?.progress.find(progress => progress.id_project === id_project);
+    const lenguage = progress?.lang;
 
-    if (!project || !progress) return 0;
+    if (!project || !progress) {
+        return {
+            progress: 0,
+            percentaje: 0,
+            lenguage: 'none'
+        }; // retorna 0 si no existe el proyecto o el progreso
+    }
 
     // const length = getProjectsContent(project);
     const length = 10;
 
-    return Math.round((progress / length) * 100); // retorna el porcentaje
+    return {
+        progress: progress?.status,
+        percentaje: Math.round((progress?.status / length) * 100),
+        lenguage: lenguage ?? 'none'
+    }; // retorna el porcentaje
 }
