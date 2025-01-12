884a516e1ac0f7a17f996be9efcaa02d4d27e2b9
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-12 10:55:41 -0600

    Refactor getProgressProjects function to fetch user progress from Supabase and return associated projects

--- src/utils/getProgressProjects.ts
+++ src/utils/getProgressProjects.ts
@@ -1,11 +1,31 @@
 import profiles from "@/db/profiles";
+import { supabase } from "@/lib/supabase";
 import getProjects from "@/utils/getProjects";
 
 const Projects = await getProjects();
 
-export default function (user_name: string) {
-    const profile = profiles.find((profile) => profile.user_name === user_name);    
-    const progress_projects = profile?.progress?.map((progress) => Projects?.find((project) => project.id === progress.id_project));
+export default async function (user_name: string) {
+    let { data: User } = await supabase
+        .from("users")
+        .select("user_name, progress")
+        .eq("user_name", user_name)
+        .single();
+    const progress = User?.progress;
 
-    return progress_projects;
+    if (!progress) return [];
+
+    let { data: Projects } = await supabase
+        .from("projects")
+        .select("icon, title, description, href, id")
+
+    const found = Projects?.filter((project) => {
+        for (let p of progress) {
+            if (p.id_project === project.id) {
+                return true;
+            }
+        }
+    });
+
+    if (!found) return [];
+    return found;
 }
