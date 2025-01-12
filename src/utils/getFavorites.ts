7f75c859ee4414f3ba0834e9856f78a3d6c23587
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-12 10:59:57 -0600

    Fetch user favorites from Supabase in getFavorites utility function

--- src/utils/getFavorites.ts
+++ src/utils/getFavorites.ts
@@ -2,7 +2,13 @@ import { supabase } from "@/lib/supabase";
 import profiles from "@/db/profiles";
 
 export default async function (user_name: string) {
-    const favorites = profiles.find((profile) => profile.user_name === user_name)?.favorites;
+    let { data: user } = await supabase
+        .from("users")
+        .select("favorites")
+        .eq("user_name", user_name)
+        .single();
+
+    const favorites = user?.favorites;
 
     if (!favorites) return [];
 
