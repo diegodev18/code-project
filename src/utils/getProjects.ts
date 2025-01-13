38bf8da8d7ba232ba16ddbd21d7f86e374a597f5
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-13 07:12:55 -0600

    Filter projects to include only those with a public status

--- src/utils/getProjects.ts
+++ src/utils/getProjects.ts
@@ -4,6 +4,7 @@ export default async function () {
     let { data: projects, error } = await supabase
         .from("projects")
         .select("*")
+        .eq("status", "public")
         .order("num", { ascending: false });
     if (error) {
         throw new Error(error.message);
