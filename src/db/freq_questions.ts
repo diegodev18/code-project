d164f53ad8731de161669e745598dc03195b3ca6
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-02-28 09:44:59 -0600

    Update FAQ titles for improved clarity and add new questions regarding API limits and project downloads

--- src/db/freq_questions.ts
+++ src/db/freq_questions.ts
@@ -1,30 +1,38 @@
 export default [
     {
-        title: "Como puedo acceder a mi cuenta?",
+        title: "¿Como puedo acceder a mi cuenta?",
         content: "Para acceder a tu cuenta, debes iniciar session con github, presionando el boton con el icono de la persona en la esquina superior derecha."
     },
     {
-        title: "Como accedo a un proyecto?",
+        title: "¿Como accedo a un proyecto?",
         content: "Para acceder a un proyecto, debes buscarlo en la barra de busqueda y presionar en su tarjeta."
     },
     {
-        title: "Es gratis?",
+        title: "¿Es gratis?",
         content: "Si, Code Project y todos los proyectos que se encuentran en la plataforma son completamente gratis. Tambien, puedes contribuir a la plataforma via patricinio en GitHub en \"codeproject.site/link/pay\"."
     },
     {
-        title: "Puedo subir mis propios proyectos?",
+        title: "¿Puedo subir mis propios proyectos?",
         content: "Si, puedes subir tus proyectos. Estamos trabajando en una manera de subir proyectos de manera mas sencilla y comoda. Por ahora, puedes subir tus proyectos vía Pull Request en el repositorio oficial GitHub, encuentralo con \"codeproject.site/link/docs\"."
     },
     {
-        title: "Como puedo contribuir a la plataforma?",
+        title: "¿Como puedo contribuir a la plataforma?",
         content: "Puedes contribuir de manera monetaria via patricinio en GitHub en \"codeproject.site/link/pay\". Tambien puedes contribuir subiendo tus propios proyectos via Pull Request en el repositorio oficial GitHub, encuentralo con \"codeproject.site/link/docs\"."
     },
     {
-        title: "Como puedo contactar a los creadores de la plataforma?",
+        title: "¿Como puedo contactar a los creadores de la plataforma?",
         content: "Si, claro. Puedes contactar a los creadores de la plataforma via twitter en \"@diegodev018\" o por formulario via \"codeproject.site/link/form\"."
     },
     {
-        title: "Como puedo reportar un error?",
+        title: "¿Como puedo reportar un error?",
         content: "Puedes reportar un error via formulario en \"codeproject.site/link/form\" o via GitHub en el repositorio oficial de issues, encuentralo con \"codeproject.site/link/docs\"."
+    },
+    {
+        title: "Al tratar de acceder a un proyecto, me aparece un mensaje que dice \"Alcanzaste el límite de peticiones a la API de los DOCs.\". ¿Que hago?",
+        content: "Este mensaje aparece cuando se alcanza el limite de peticiones a la API de los DOCs. Puedes esperar unos minutos y volver a intentar acceder al proyecto. Si el problema persiste, puedes contactar a los desarrolladores de la plataforma por formulario via \"codeproject.site/link/form\". Si quieres apoyar en una solucion mas optima, puedes contribuir via patricinio en GitHub en \"codeproject.site/link/pay\"."
+    },
+    {
+        title: "¿Puedo descargar los proyectos?",
+        content: "Por el momento, no puedes hacerlo. Estamos trabajando en una manera de poder descargar los proyectos de manera sencilla y comoda. Por ahora, puedes descargar los archivos MarkDown de los proyectos via GitHub en el repositorio oficial, encuentralo con \"codeproject.site/link/docs\" gracias a las contribuciones de la comunidad."
     }
 ]
