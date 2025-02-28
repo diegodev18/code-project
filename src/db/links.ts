0953b3f3f40634c01167532d458fae3d3187454d
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-02-28 09:11:42 -0600

    Refactor footer links to use centralized link management

--- src/db/links.ts
+++ src/db/links.ts
@@ -1,18 +1,18 @@
 
 const links: { [key: string]: { title: string, href: string } } = {
-    "docs": {
+    docs: {
         title: "Repositorio de Documentación",
         href: "https://github.com/diegodev18/code-project-docs"
     },
-    "sponsor": {
+    sponsor: {
         title: "Pagina de Sponsor en Github",
         href: "https://github.com/sponsors/diegodev18"
     },
-    "form": {
+    form: {
         title: "Formulario de Feedback",
         href: "https://tally.so/r/mBAl87"
     },
-    "founder": {
+    founder: {
         title: "Fundador del Proyecto",
         href: "https://www.diego18.pro/"
     },
