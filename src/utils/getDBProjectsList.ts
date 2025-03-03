6207f194a7e7de4941afc2c2668add9d0cba4a99
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-03-03 08:06:04 -0600

    Refactor getDBProjectsList utility for improved readability and error handling

--- src/utils/getDBProjectsList.ts
+++ src/utils/getDBProjectsList.ts
@@ -1,12 +1,13 @@
 import { getGithubDirContent } from "@/utils/getGithubDocContent";
 
 export default async function (id: string, lang: string) {
-    const { data, error } = await getGithubDirContent("diegodev18", "code-project-docs", [id, lang])
+    const { data, error } = await getGithubDirContent(
+        "diegodev18",
+        "code-project-docs",
+        [id, lang]
+    );
 
-    if (error) {
-        // console.error(error);
-        return ["403"];
-    }
+    if (error) return ["403"];
 
     return data.filesName;
 }
