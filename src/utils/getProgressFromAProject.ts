c82a1f6b4057ba2bd29b1229787036ac62a55185
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-03-03 08:03:44 -0600

    Refactor getProgressFromAProject utility to improve project fetching and utilize getUserProgress for enhanced data handling

--- src/utils/getProgressFromAProject.ts
+++ src/utils/getProgressFromAProject.ts
@@ -1,33 +1,27 @@
-import { supabase } from "@/lib/supabase";
-import getProjects from "@/utils/getAllProjects";
+import { allProjects } from "@/utils/getAllProjects";
 import { getGithubDirContent } from "@/utils/getGithubDocContent";
+import getUserProgress from "./getUserProgress";
 
-const Projects = await getProjects();
+const progressObj = {
+    progress: 0,
+    percentaje: 0,
+    lenguage: 'none'
+}
 
 export default async function (id_project: string, uuid: string): Promise<{ progress: number, percentaje: number, lenguage: string }> {
-    if (!Projects) {
-        throw new Error("Projects is null");
-    }
-    let { data: progressTable } = await supabase
-        .from('users')
-        .select('progress')
-        .eq('id', uuid)
-        .single();
+    if (!allProjects) throw new Error("Projects is null");
 
-    let progress = progressTable?.progress.find((progress: any) => progress.id_project === id_project);
+    const progressTable = await getUserProgress(uuid);
+    if (!progressTable) return progressObj;
+
+    let progress = progressTable?.find((progress: any) => progress.id_project === id_project);
     const lenguage = progress?.lang;
 
-    const project = Projects.find(project => project.id === id_project);
+    const project = allProjects.find(project => project.id === id_project);
 
-    if (!project || !progress) {
-        return {
-            progress: 0,
-            percentaje: 0,
-            lenguage: 'none'
-        }; // retorna 0 si no existe el proyecto o el progreso
-    }
+    if (!project || !progress) return progressObj;
 
-    const { data, error } = await getGithubDirContent("diegodev18", "code-project-docs", [project.id, lenguage]);
+    const { data } = await getGithubDirContent("diegodev18", "code-project-docs", [project.id, lenguage]);
     const length = data?.filesLength ?? 0;
 
     return {
