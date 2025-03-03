ee0f500754e07a75ca0fe1eb5ddc6f49a0bb397f
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-03-02 18:51:58 -0600

    Refactor Tailwind CSS typography settings for improved consistency and clarity

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,21 +15,15 @@ export default {
 			typography: {
 				DEFAULT: {
 					css: {
-						h1: { marginBottom: '0.5rem', fontWeight: '600' },
-						h2: { marginBottom: '0.5rem', marginTop: '0.8rem' },
-						h3: { marginBottom: '0.5rem', marginTop: '0.8rem' },
-						h4: { marginBottom: '0.5rem', marginTop: '0.8rem' },
-						p: { marginTop: '0.8rem', marginBottom: '0.5rem' },
-						pre: { marginTop: '0.8rem', marginBottom: '0.8rem', cursor: 'pointer', fontWeight: '600' },
+						'*': { marginBottom: '0.5rem', marginTop: '0.8rem' },
+						h1: { fontWeight: '600' },
+						pre: { marginBottom: '0.8rem', cursor: 'pointer', fontWeight: '600' },
 						code: { color: '#fde047', backgroundColor: '#0c0c0c', paddingLeft: '0.3rem', paddingRight: '0.3rem', paddingTop: '0.2rem', paddingBottom: '0.1rem', borderRadius: '0.25rem' },
-						'code::before': { content: '""' },
-						'code::after': { content: '""' },
+						'code::before, code::after': { content: '""' },
 						'a': { color: '#fde047' },
 						'a:hover': { color: '#fde047' },
-						'ul': { marginBottom: '0.5rem', marginTop: '0rem' },
-						'ol': { marginBottom: '0.5rem', marginTop: '0rem' },
-						'ul > li': { marginLeft: '1.5rem', marginTop: '0.5rem', marginBottom: '0.5rem' },
-						'ol > li': { marginLeft: '1.5rem', marginTop: '0.5rem', marginBottom: '0.5rem' },
+						'ul, ol': { marginTop: '0rem' },
+						'ul > li, ol > li': { marginLeft: '1.5rem', marginTop: '0.5rem',  },
 					}
 				}
 			}
