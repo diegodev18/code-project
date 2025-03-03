11256e6a6488ebf9c3439e452615d0c381ebd274
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-03-03 08:02:58 -0600

    Refactor getAllProjects utility for improved data fetching and error handling

--- src/utils/getAllProjects.ts
+++ src/utils/getAllProjects.ts
@@ -1,19 +1,10 @@
 import { supabase } from "@/lib/supabase";
 
-export default async function (): Promise<{ num: number; id: string; created_at: Date; title: string; description: string; icon: string; lenguages: string[]; href: string; tags: string[]; status: string }[]> {
-    let { data: projects, error } = await supabase // Trae la lista de proyectos públicos
-        .from("projects")
-        .select("*")
-        .eq("status", "public")
-        .order("created_at", { ascending: false });
+export const { data: allProjects, error } = await supabase // Trae la lista de proyectos públicos
+    .from("projects")
+    .select("*")
+    .eq("status", "public")
+    .order("created_at", { ascending: false });
 
-    if (error) {
-        console.error("Error fetching projects in getProjects.ts", error);
-        return [];
-    }
-    if (!projects) return [];
 
-    // console.log(projects);
 
-    return projects;
-}
