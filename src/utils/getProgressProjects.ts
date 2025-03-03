3af9bb5181a65a3cfd78818acd9a203d96845171
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-03-03 07:56:34 -0600

    Refactor getProgressProjects utility to utilize getUserProgress for improved data fetching

--- src/utils/getProgressProjects.ts
+++ src/utils/getProgressProjects.ts
@@ -1,14 +1,9 @@
 import { supabase } from "@/lib/supabase";
+import getUserProgress from "@/utils/getUserProgress";
 
-export default async function (uuid: string) {
-    let { data: User } = await supabase // Trae la lista de proyectos en progreso (y completados) del usuario
-        .from("users")
-        .select("progress")
-        .eq("id", uuid)
-        .single();
-        
-    if (!User?.progress) return [];
-    const progressProjects = User?.progress;
+export default async function (uuid: string) {        
+    const progressProjects = await getUserProgress(uuid);
+    if (!progressProjects) return [];
 
     const found = await Promise.all(progressProjects.map(async (onProgress: any) => { // Trae la información de cada proyecto en progreso
         const { data: project } = await supabase // Busca el proyecto en la tabla de proyectos
