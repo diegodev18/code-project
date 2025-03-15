100e37cf633101b47fed3b1f937a00e2720b1d5f
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-03-14 19:16:44 -0600

    Add sitemap()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.codeproject.site/",
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()],
 });
