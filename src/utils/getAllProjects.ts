44bd64b7237a823d20214266930baf5c55112185
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-20 21:42:55 -0600

    Add conditional if is prod to get all status projects

--- src/utils/getAllProjects.ts
+++ src/utils/getAllProjects.ts
@@ -1,8 +1,12 @@
 import { supabase } from "@/lib/supabase";
 
-export const { data: allProjects, error } =
-  (await supabase // Trae la lista de proyectos públicos
-    .from("projects")
-    .select("*")
-    .eq("status", "public")
-    .order("created_at", { ascending: false })) ?? [];
+export const { data: allProjects, error } = import.meta.env.DEV
+  ? await supabase // Trae la lista de proyectos públicos
+      .from("projects")
+      .select("*")
+      .order("created_at", { ascending: false })
+  : (await supabase // Trae la lista de proyectos públicos
+      .from("projects")
+      .select("*")
+      .eq("status", "public")
+      .order("created_at", { ascending: false })) || [];
