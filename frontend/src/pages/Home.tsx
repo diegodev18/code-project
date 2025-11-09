0b6bf8679bdda4b9832478b699e0d953ef4beb6c
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-11-08 22:09:06 -0600

    feat: implement Home component and update App to use React Router

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <h1 className="flex flex-col items-center justify-center h-screen bg-black">
+      <span className="text-2xl font-semibold text-white">Home Page</span>
+      <span className="font-semibold text-yellow-300">{"<codeproject />"}</span>
+    </h1>
+  );
+}
